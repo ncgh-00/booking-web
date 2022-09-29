eba2c6eb6def6684a03f500afb77190859f0d973
phongbinhtran <phongbinh967@gmail.com>
2022-09-29 21:41:13 +0700

    commit thu ne

---
+++ src/main/java/com/group3/trividi/model/City.java
@@ -0,0 +1,43 @@
+package com.group3.trividi.model;
+
+public class City {
+    private int id;
+    private String name;
+    private String link_images;
+
+    public City(int id, String name, String link_images) {
+        this.id = id;
+        this.name = name;
+        this.link_images = link_images;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+
+
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getLink_images() {
+        return link_images;
+    }
+
+    public void setLink_images(String link_images) {
+        this.link_images = link_images;
+    }
+
+
+
+}
