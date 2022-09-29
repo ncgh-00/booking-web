a7d71159564ba6ef6b8f83f5ff2822d6e0531b9a
ncgh-00 <nguyencaogiahuy.00@gmail.com>
2022-09-29 16:42:56 +0700

    hhhhhh

--- src/main/java/com/group3/trividi/context/DBContext.java
+++ src/main/java/com/group3/trividi/context/DBContext.java
@@ -22,6 +22,7 @@ public class DBContext {
 //alooooooo
     /*USE BELOW METHOD FOR YOUR DATABASE CONNECTION FOR BOTH SINGLE AND MULTILPE SQL SERVER INSTANCE(s)*/
     /*DO NOT EDIT THE BELOW METHOD, YOU MUST USE ONLY THIS ONE FOR YOUR DATABASE CONNECTION*/
+    // cho binh
     public Connection getConnection()throws Exception {
         String url = "jdbc:sqlserver://"+serverName+":"+portNumber + "\\" + instance +";databaseName="+dbName;
         if(instance == null || instance.trim().isEmpty())
