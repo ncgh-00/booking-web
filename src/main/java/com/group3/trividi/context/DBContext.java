8bd3b6a97e923929a81fe7ddf3da0f995aa57623
lndt123 <chinhleece160936@gmail.com>
2022-09-29 17:08:17 +0700

    ccc

--- src/main/java/com/group3/trividi/context/DBContext.java
+++ src/main/java/com/group3/trividi/context/DBContext.java
@@ -32,8 +32,9 @@ public class DBContext {
     }
     /*Insert your other code right after this comment*/
     /*Change/update information of your database connection, DO NOT change name of instance variables in this class*/
-    private final String serverName = "DESKTOP-RTOR6G3";
+    private final String serverName = "localhost";
     private final String dbName = "Web_Booking";
+//    trinh ne
     private final String portNumber = "1433";
     private final String instance="";//LEAVE THIS ONE EMPTY IF YOUR SQL IS A SINGLE INSTANCE
     private final String userID = "sa";
